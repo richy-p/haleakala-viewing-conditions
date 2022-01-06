e0126a59a1cf912c87908f30bf8a4ea4ee6be4f3
Richy <rpeterson0608@gmail.com>
2022-01-05 21:58:48 -1000

    code modification to run prep data or hypothesis test individually

--- src/myplots.py
+++ src/myplots.py
@@ -4,6 +4,13 @@ import os
 plt.style.use('ggplot')
 plt.rcParams.update({'font.size':16,'axes.labelcolor': '#000000','ytick.color': '#000000','xtick.color': '#000000'})
 
+green = '#63d297'
+yellow = '#fff176'
+red = '#ff5252'
+colors = [green,yellow,red]
+conditions = ['Green','Yellow','Red']
+
+
 def avg_daily_hours(df,save_path=None):
     '''
     Creates a bar chart showing the average daily weather status for green, yellow and red weather for all months.
@@ -13,16 +20,17 @@ def avg_daily_hours(df,save_path=None):
     save_path : str
         Location to save figure.
     '''
-    fig,ax = plt.subplots(figsize=(6,4))
-    df.drop('year',axis=1).groupby(['month']).mean().plot.bar(color=['g','y','r'],ax=ax)
-    ax.set_ylim(0,24)
+    fig,ax = plt.subplots(figsize=(6,5))
+    df.drop('year',axis=1).groupby(['month']).mean().plot.bar(color=colors,ax=ax,stacked=True)
+    ax.set_ylim(0,27)
     ax.set_xlabel(None)
     ax.set_ylabel('Hours')
-    ax.set_title('Average Daily Weather Status')
-    ax.legend(ncol=3,fontsize=12)
+    ax.set_title('Mean Daily Weather Status')
+    ax.set_yticks(range(0,25,4))
+    ax.legend(ncol=3,fontsize=14,loc='upper center', bbox_to_anchor=(.5,1.02))
     fig.tight_layout()
     if save_path:
-        fig.savefig(os.path.join(save_path,'average_daily_status.png'),dpi=300)
+        fig.savefig(os.path.join(save_path,'mean_monthly_hours.png'),dpi=300)
 
 def daily_green_weather_over_time(df,months='All',show_comb_avg=False,save_path=None,**kwargs):
     '''
@@ -112,7 +120,7 @@ def plot_combined_distribution_wx(df,statuses='All',save_path=None):
         status_list = ['Green','Yellow','Red']
     elif type(statuses) == str:
         status_list = statuses.split()
-    color_dict = {'Green': 'g', 'Yellow': 'y', 'Red': 'r'}
+    color_dict = {'Green': green, 'Yellow': yellow, 'Red': red}
     fig,ax = plt.subplots(figsize=(10,5))
     for status in status_list:
         df[status].hist(bins=24,ax=ax,alpha=0.3,color=color_dict[status],label=status)
@@ -124,6 +132,30 @@ def plot_combined_distribution_wx(df,statuses='All',save_path=None):
     if save_path:
         fig.savefig(os.path.join(save_path,'combined_weather_distribution.png'),dpi=300)
 
+def plot_combined_distribution_wx_stacked(df,save_path=None):
+    '''
+    Plot the combined distribution all months.
+    Parameters
+    ----------
+    df : DataFrame
+    save_path : str
+        Location to save the figure to. 
+    '''
+    fig,axs = plt.subplots(3,1,figsize=(8,10),sharex=True)
+    conditions = ['Green','Yellow','Red']
+    colors = [green,yellow,red]
+    for idx,ax in enumerate(axs.flatten()):
+        df[conditions[idx]].hist(ax=ax,bins=24,color=colors[idx])
+        ax.set_xlim(0,24)
+        ax.set_ylim(0,3100)
+        ax.set_title(conditions[idx],y=.85)
+
+    axs[1].set_ylabel('Frequency (# days)')
+    axs[2].set_xlabel('Hours')
+    fig.suptitle('Distribution of Weather Conditions')
+    fig.tight_layout()   
+    if save_path:
+        fig.savefig(os.path.join(save_path,'combined_weather_distribution_stacked.png'),dpi=300)   
 
 if __name__ == "__main__":
     pass
--- src/prep_data.py
+++ src/prep_data.py
@@ -1,3 +1,5 @@
+import utilities as ut
+
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
@@ -32,6 +34,68 @@ def get_csv_file_links(base_url):
     if not csv_urls:
         print(f'No csv files found at {base_url}')
     return csv_urls
+
+def get_and_prep_data(url,range_limits,thresholds,save_results=True,save_path='data/',return_df=False):
+    '''
+    Pipeline to load raw csv file from the IfA archive, clean it, and prepare it by calculating the hours of green, yellow, and red weather.
+    Parameters
+    ----------
+    url : str
+        URL of the CSV file to read
+    range_limits : dict
+        lower and upper limits for each columns except the date_time
+    thresholds : dict
+        Must contain the columns above as keys with the values being a tuple with the green and red weather threshold values.
+    save_results : bool
+        if true, results saved in location given by save_path
+    save_path : str
+        location to store the df with the status hours
+    return_df : bool
+        if true, returns the df with the calculated status hours
+    '''
+    # data column names
+    column_names = ['date_time','temperature','pressure','humidity','wind_speed','wind_direction','visibility','co2','insolation','vertical_wind_speed','precipitation','10min','dewpoint']
+    columns_of_interest = ['date_time','temperature','humidity','wind_speed','visibility','precipitation','dewpoint','10min']
+
+    year = url.split('/')[-1].split('.')[0]
+    try:
+        # read data
+        df = read_data_of_interest(url,column_names,columns_of_interest)
+        print(f'{year} data read, processing now.')
+    except:
+        print(f'Failed to read data for {year} at: {url} ')
+
+    # record the number of NaNs for awareness (possible later anaylsis)
+    with open(os.path.join(save_path,'NaN_info.txt'),'a') as f:
+       print('',file=f) # print an empty line to break up the years
+       count_NaNs(df,f)
+
+    # check for reasonable values
+    remove_unreasonable_measurements(df,range_limits,inplace=True)
+    
+    # split wind into sustaind and gusts
+    df = determine_wind_sust_and_gust(df)
+
+    # add delta dew point
+    df['dewpoint_delta'] = df['temperature'] - df['dewpoint']
+
+    # convert thresholds to status
+    df['status'] = get_weather_status(df,thresholds)
+
+    # record NaNs after prepping data (will now include values removed outside limit range)
+    with open(os.path.join(save_path,'NaN_info.txt'),'a') as f:
+       print('After prep',file=f)
+       count_NaNs(df,f)
+
+    # make new df with daily hours
+    df_status_hours = generate_status_hours_df(df)
+
+    print(f'{year} data prep complete\n')
+    # save new df
+    if save_results:
+        ut.save_df_to_csv(df_status_hours,f'status_hours_{year}',save_path=save_path)
+    if return_df:
+        return df_status_hours
 
 def get_specific_year(year,url_list):
     '''
@@ -232,4 +296,70 @@ def add_month_year_columns(df):
     pass
 
 if __name__ == "__main__":
-    pass
+    # set testing to True to just use smaller data sets from 1994-2005
+    testing = False
+    
+    # skip years that don't have data (1993) or have formatting issues (2020-2021)
+    skip_years = {1993,2020,2021}
+    if testing:
+        # use smaller data set to run for testing
+        skip_years.update(set(range(2006,2020,1)))
+        
+
+    # Establish required info
+    # link for data files
+    base_url = "http://kopiko.ifa.hawaii.edu/weather/archivedata/"
+    
+    # Define reasonable ranges for each column
+    acceptable_ranges = {
+        'temperature': (-273,40),
+        'humidity': (0,100),
+        'wind_speed': (0,100),
+        'visibility': (0,100000),
+        'precipitation': (0,100),
+        'dewpoint': (-273,40)
+        }
+    # Define the thresholds for ('Green', 'Red') weather - plan to use config file in future
+    thresholds = {
+            'humidity': (75,85),
+            'wind_sust': (10,12),
+            'wind_gust': (15,15),
+            'visibility': (50000,40000),
+            'precipitation': (0,0),
+            'dewpoint_delta': (6,3)
+            }
+
+    # Run 
+    # make directory for current run - allows for running multiple tests 
+    data_dir = ut.make_numbered_directory(parent_dir='data',base_name='prepped_data')
+
+
+
+    # get list of all data file urls
+    csv_urls = get_csv_file_links(base_url)
+
+    # prep all data
+    # prep_all_available_data(csv_urls,acceptable_ranges,thresholds,save_results=True,save_path=data_dir) 
+    for url in csv_urls:
+        year = url.split('/')[-1].split('.')[0]
+        if int(year) in skip_years:
+            continue
+        # if prepped data file already exist for that year skip it
+        elif ut.prepped_data_exists(year,base_path=data_dir):
+            print(f'{year} data already prepped.')
+            continue
+        else:
+            get_and_prep_data(url,acceptable_ranges,thresholds,save_results=True,save_path=data_dir)
+            
+    # record the set up info to a file
+    years = ut.get_years(data_dir)
+    with open(os.path.join(data_dir,'run_info.txt'),'a') as f:
+        ut.record_setup(thresholds,acceptable_ranges,years,f)
+
+
+    # combine the daily status hours for all years into one df
+    df = combine_status_hour_dfs(base_path=data_dir)
+    df = normalize_daily_hours_to_24(df)
+    add_month_year_columns(df)
+    ut.save_df_to_csv(df,'combined_status_hours',data_dir)
+
